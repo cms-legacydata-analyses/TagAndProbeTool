ba164c97f9d1c03bf5ac9793f71f0276fda27c73
allanjales <jales.allan@gmail.com>
2021-07-26 22:22:32 -0300

    Updated efficiency for full compatibility

--- efficiency_tools/fitting/efficiency.cpp
+++ efficiency_tools/fitting/efficiency.cpp
@@ -11,16 +11,14 @@ string MuonId   = "trackerMuon";
 //string MuonId   = "globalMuon";
 
 //Which quantity do you want to use?
-//string quantity = "Pt";     double bins[] = {0., 2.0, 3.4, 4.0, 4.4, 4.7, 5.0, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.3, 9.5, 13.0, 17.0, 40.};
+string quantity = "Pt";     double bins[] = {0., 2.0, 3.4, 4.0, 4.4, 4.7, 5.0, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.3, 9.5, 13.0, 17.0, 40.};
 //string quantity = "Eta";    double bins[] = {-2.4, -1.8, -1.4, -1.2, -1.0, -0.8, -0.5, -0.2, 0, 0.2, 0.5, 0.8, 1.0, 1.2, 1.4, 1.8, 2.4};
 //string quantity = "Phi";    double bins[] = {-3.0, -1.8, -1.6, -1.2, -1.0, -0.7, -0.4, -0.2, 0, 0.2, 0.4, 0.7, 1.0, 1.2, 1.6, 1.8, 3.0};
-string quantity = "Pt";     double bins[] = {0.0, 2.0, 3.4, 4.0, 5.0, 6.0, 8.0, 10.0, 40.};
-//string quantity = "Eta";    double bins[] = {0.0, 0.4, 0.6, 0.95, 1.2, 1.4, 1.6, 1.8, 2.1};
 
 void efficiency()
 {
 	//Path where is going to save results png for every bin 
-	const char* path_bins_fit_folder = "results/bins_fit/";
+	const char* path_bins_fit_folder = "results/bins_fit/efficiency/";
 	create_folder(path_bins_fit_folder, true);
 
 	// Loop for every bin and fit it
@@ -29,8 +27,8 @@ void efficiency()
 	for (int i = 0; i < bin_n; i++)
 	{
 		//Creates conditions
-		string conditions = string(    "ProbeMuon_" + quantity + ">" + to_string(bins[i]  ));
-		conditions +=       string(" && ProbeMuon_" + quantity + "<" + to_string(bins[i+1]));
+		string conditions = string(    "ProbeMuon_" + quantity + ">=" + to_string(bins[i]  ));
+		conditions +=       string(" && ProbeMuon_" + quantity + "< " + to_string(bins[i+1]));
 
 		//Stores [yield_all, yield_pass, err_all, err_pass]
 		yields_n_errs[i] = doFit(conditions, MuonId, path_bins_fit_folder);
@@ -47,8 +45,8 @@ void efficiency()
 	//Create histograms
 	generatedFile->mkdir("histograms/");
 	generatedFile->   cd("histograms/");
-	TH1D *yield_all  = make_hist("ALL" , yields_n_errs, 0, bin_n, bins);
-	TH1D *yield_pass = make_hist("PASS", yields_n_errs, 1, bin_n, bins);
+	TH1D *yield_all  = make_hist("ALL" , yields_n_errs, 0, bins, bin_n, quantity);
+	TH1D *yield_pass = make_hist("PASS", yields_n_errs, 1, bins, bin_n, quantity);
 	
 	//Create efficiencies
 	generatedFile->   cd("/");
